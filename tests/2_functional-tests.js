fb3eabbe2e5220ef7c1b39f40fed2a35f89dfee3
temwani-msiska <138024035+temwani-msiska@users.noreply.github.com>
2023-08-10 09:01:23 +0200

    functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,68 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  // Test for viewing one stock
+  test('Viewing one stock', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL' }) // Replace with the actual stock symbol
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        // Add more assertions to check the response as needed
+        done();
+      });
+  });
+  // Test for viewing one stock and liking it
+  test('Viewing one stock and liking it', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: true }) // Replace with the actual stock symbol
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        // Add more assertions to check the response as needed
+        done();
+      });
+  });
 
+  // Test for viewing the same stock and liking it again
+  test('Viewing the same stock and liking it again', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: true }) // Replace with the actual stock symbol
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        // Add more assertions to check the response as needed
+        done();
+      });
+  });
+
+  // Test for viewing two stocks
+  test('Viewing two stocks', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['AAPL', 'GOOG'] }) // Replace with the actual stock symbols
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        // Add more assertions to check the response as needed
+        done();
+      });
+  });
+
+  // Test for viewing two stocks and liking them
+  test('Viewing two stocks and liking them', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['AAPL', 'GOOG'], like: true }) // Replace with the actual stock symbols
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        // Add more assertions to check the response as needed
+        done();
+      });
+  });
 });
+
